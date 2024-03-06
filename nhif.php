27dbbfb1b495997dbc01ccb310334ec5d0451f5e
James-wasonga <jameswasonga22@gmail.com>
2024-03-06 16:58:39 +0300

    php tutorials

--- nhif.php
+++ nhif.php
@@ -1,1 +1,17 @@
 
+<?php
+	$basicpay = 40000;
+    $nssf = 200; //national social security insurance fund
+	$nhif = 400; //national health insurance fund
+
+	function pension(){
+		global $basicpay;	//the variable will be able to access the initialisation
+		$rate = 5;
+		$pension = ($rate/100)*$basicpay;
+		return $pension;
+	}
+	
+	$deductions=$nssf+$nhif+pension();		//total deductions from the salary
+	
+	echo $deductions;
+?>
